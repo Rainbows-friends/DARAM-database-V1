f780dfd9373c23a5c19f7e04844137b1b2efd85b
snowykte0426 <snowykte0426@naver.com>
2024-11-14 23:33:53 +0900

    feat: 2024학년도 2학기 1학년 호실정보 쿼리문 작성
    2024학년도 2학기 남자기숙사 1학년 호실정보 쿼리문을 작성하였습니다

---
+++ 2024-g1.sql
@@ -0,0 +1,112 @@
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김동현', 1, 3, 2, 3, 301, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김재관', 1, 3, 6, 3, 301, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이산', 1, 3, 10, 3, 301, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김민종', 1, 3, 3, 3, 302, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김한주', 1, 3, 7, 3, 302, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이평강', 1, 3, 11, 3, 302, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김승찬', 1, 3, 5, 3, 303, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('박정우', 1, 3, 8, 3, 303, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('정현태', 1, 3, 14, 3, 303, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('최준영', 1, 3, 16, 3, 303, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('권재헌', 1, 4, 1, 3, 304, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김홍준', 1, 4, 5, 3, 304, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('서경주', 1, 4, 9, 3, 304, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김태은', 1, 4, 4, 3, 305, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이용표', 1, 4, 13, 3, 305, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('전준연', 1, 4, 16, 3, 305, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('박승일', 1, 4, 6, 3, 306, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('송재욱', 1, 4, 10, 3, 306, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이주언', 1, 4, 14, 3, 306, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('최민준', 1, 4, 17, 3, 306, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('정재원', 1, 3, 13, 3, 307, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('서우주', 1, 3, 9, 3, 307, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('정세준', 1, 3, 12, 3, 307, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('임시현', 1, 4, 15, 3, 307, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('배용빈', 1, 4, 8, 3, 308, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김명현', 1, 4, 2, 3, 308, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('허은서', 1, 3, 17, 3, 308, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('박현민', 1, 4, 7, 3, 309, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이상혁', 1, 4, 12, 3, 309, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('황지훈', 1, 4, 18, 3, 309, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김담율', 1, 1, 1, 3, 310, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('문승덕', 1, 1, 4, 3, 310, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이세민', 1, 1, 10, 3, 310, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김민균', 1, 1, 2, 3, 311, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('전선우', 1, 1, 13, 3, 311, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김민준', 1, 2, 1, 3, 311, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('조수민', 1, 2, 17, 3, 312, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김지훈', 1, 2, 3, 3, 312, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('서정민', 1, 2, 7, 3, 312, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('박찬울', 1, 1, 6, 3, 313, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이영서', 1, 1, 11, 3, 313, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('하창혁', 1, 1, 16, 3, 313, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김승현', 1, 1, 3, 3, 314, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('임한솔', 1, 1, 12, 3, 314, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('함성우', 1, 1, 17, 3, 314, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이서준', 1, 1, 8, 3, 314, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('김준혁', 1, 2, 2, 3, 315, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이시우', 1, 2, 10, 3, 315, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('임지훈', 1, 2, 13, 3, 315, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('한승재', 1, 2, 18, 3, 315, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('나현욱', 1, 2, 4, 3, 316, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이은아', 1, 2, 11, 3, 316, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('장경훈', 1, 2, 14, 3, 316, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('모태환', 1, 2, 5, 3, 317, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('이준건', 1, 2, 12, 3, 317, 'USER');
+INSERT INTO members (name, grade, class, number, floor, room, role)
+VALUES ('정창진', 1, 2, 15, 3, 317, 'USER');
