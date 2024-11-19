866a66fe60f2cb813c335dd37701691ea259a401
snowykte0426 <snowykte0426@naver.com>
2024-11-19 17:58:27 +0900

    feat: ``late_table_init`` 쿼리문 작성
    Member의 지각 횟수를 저장하는 테이블의 초기화 쿼리문을 작성하였습니다

---
+++ late_table_init.sql
@@ -0,0 +1,402 @@
+INSERT INTO member_late_table (member_id, late)
+VALUES (1, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (2, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (3, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (4, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (5, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (6, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (7, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (8, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (9, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (10, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (11, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (12, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (13, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (14, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (15, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (16, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (17, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (18, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (19, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (20, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (21, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (22, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (23, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (24, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (25, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (26, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (27, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (28, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (29, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (30, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (31, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (32, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (33, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (34, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (35, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (36, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (37, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (38, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (39, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (40, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (41, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (42, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (43, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (44, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (45, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (46, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (47, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (48, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (49, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (50, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (51, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (52, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (53, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (54, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (55, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (56, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (57, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (58, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (59, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (60, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (61, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (62, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (63, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (64, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (65, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (66, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (67, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (68, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (69, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (70, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (71, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (72, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (73, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (74, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (75, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (76, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (77, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (78, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (79, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (80, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (81, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (82, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (83, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (84, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (85, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (86, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (87, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (88, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (89, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (90, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (91, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (92, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (93, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (94, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (95, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (96, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (97, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (98, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (99, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (100, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (101, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (102, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (103, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (104, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (105, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (106, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (107, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (108, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (109, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (110, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (111, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (112, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (113, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (114, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (115, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (116, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (117, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (118, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (119, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (120, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (121, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (122, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (123, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (124, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (125, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (126, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (127, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (128, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (129, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (130, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (131, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (132, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (133, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (134, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (135, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (136, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (137, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (138, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (139, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (140, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (141, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (142, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (143, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (144, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (145, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (146, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (147, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (148, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (149, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (150, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (151, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (152, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (153, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (154, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (155, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (156, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (157, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (158, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (159, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (160, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (161, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (162, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (163, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (164, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (165, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (166, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (167, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (168, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (169, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (170, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (171, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (172, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (173, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (174, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (175, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (176, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (177, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (178, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (179, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (180, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (181, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (182, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (183, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (184, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (185, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (186, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (187, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (188, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (189, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (190, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (191, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (192, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (193, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (194, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (195, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (196, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (197, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (198, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (199, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (200, 0);
+INSERT INTO member_late_table (member_id, late)
+VALUES (201, 0);
